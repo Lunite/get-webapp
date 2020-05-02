3069597889647ece6567e7f23489be06daeacc20
George Richards <georgejjrichards@gmail.com>
2020-05-02 13:34:01 +0100

    got all the content coming through nicely

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,50 +9,39 @@
 const path = require(`path`)
 const { slash } = require(`gatsby-core-utils`)
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for WordPress pages (route : /{slug})
-// Will create pages for WordPress posts (route : /post/{slug})
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  // The “graphql” function allows us to run arbitrary
-  // queries against the local Gatsby GraphQL schema. Think of
-  // it like the site has a built-in database constructed
-  // from the fetched data that you can run queries against.
-
-  const getPostsFromCategory = category => {
-    const postsQuery = `query getAllWordpressPosts($category: String) {
-      allWordpressPost(filter: {categories: {elemMatch: {slug: {eq: $category}}}}) {
-        nodes {
-          id
-          content
-          path
-          slug
-          title
-          acf {
-            description
-            related_products
-            image {
-              source_url
-              caption
-              alt_text
-            }
-            information {
-              dc_peak
-              developer
-              inverters
-              location
-              map_url
-              modules
-            }
+  const postsQuery = `query getAllWordpressPosts($category: String) {
+    allWordpressPost(filter: {categories: {elemMatch: {slug: {eq: $category}}}}) {
+      nodes {
+        id
+        content
+        path
+        slug
+        title
+        acf {
+          description
+          related_products
+          image {
+            source_url
+            caption
+            alt_text
+          }
+          information {
+            dc_peak
+            developer
+            inverters
+            location
+            map_url
+            modules
           }
         }
       }
-    }`
+    }
+  }`
 
+  createPages = category => {
     return graphql(postsQuery, { category }).then(results => {
       if (!results.data.allWordpressPost.nodes.length) {
         return Promise.resolve()
@@ -62,10 +51,10 @@ exports.createPages = async ({ graphql, actions }) => {
 
       results.data.allWordpressPost.nodes.forEach(node => {
         createPage({
-          path: `${category}/${node.slug}`,
+          path: `${category !== "page" ? `${category}/` : ""}${node.slug}`,
           component: slash(template),
           context: {
-            content: node.context,
+            content: node.content,
             title: node.title,
             slug: node.slug,
             acf: node.acf,
@@ -76,7 +65,8 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   return Promise.all([
-    getPostsFromCategory("project"),
-    getPostsFromCategory("service"),
+    createPages("page"),
+    createPages("project"),
+    createPages("service"),
   ])
 }
---
+++ src/templates/page/index.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react"
+import PageWrapper from "~/components/layout/page-wrapper"
+
+const Project: FunctionComponent<any> = context => {
+  const { title, content, acf, slug } = context.pageContext
+
+  return <PageWrapper context={{ title, acf, slug }}>{content}</PageWrapper>
+}
+
+export default Project
