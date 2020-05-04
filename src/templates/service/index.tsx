860e5ec56df740ec3c425fd126e650f5f16ea4e8
George Richards <georgejjrichards@gmail.com>
2020-05-04 23:30:16 +0100

    adding service template file

---
+++ src/templates/service/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react"
+import Hero from "~/components/configurable/Hero"
+import Heading from "~/components/configurable/Heading"
+import Block from "~/components/configurable/Block"
+import Col6 from "~/components/grid/Col6"
+import Image from "~/components/configurable/Image"
+import InfoStrip from "~/components/configurable/InfoStrip"
+
+const Service: FunctionComponent<any> = context => {
+  const { title, acf, slug } = context.pageContext
+
+  return <></>
+}
+
+export default Service
