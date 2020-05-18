d28c725e28c93f1b2773c6008384a5fccef99f96
George Richards <georgejjrichards@gmail.com>
2020-05-18 01:53:35 +0100

    adding cookie and gtm stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,35 @@ export const wrapPageElement = ({ element }) => {
 
   return <PageWrapper context={context}>{element}</PageWrapper>
 }
+
+// const GTMNoScript = () => {
+//   return (
+//     <noscript>
+//       <iframe
+//         src="https://www.googletagmanager.com/ns.html?id=GTM-W6LJZSJ"
+//         height="0"
+//         width="0"
+//         style="display:none;visibility:hidden"
+//       ></iframe>
+//     </noscript>
+//   )
+// }
+
+// const CookieBanner = () => {
+//   const initBanner = () => {
+//     var s = document.createElement("script")
+//     s.type = "text/javascript"
+//     s.async = true
+//     s.src = "https://app.termly.io/embed.min.js"
+//     s.id = "dee08c58-630a-4f90-8bd5-8af05cf976e6"
+//     s.setAttribute("data-name", "termly-embed-banner")
+//     var x = document.getElementsByTagName("script")[0]
+//     x.parentNode.insertBefore(s, x)
+//   }
+
+//   return <script>{initBanner()}</script>
+// }
+
+// export const onRenderBody = ({ setPreBodyComponents }) => {
+//   setPreBodyComponents([CookieBanner, GTMNoScript])
+// }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+import React from "react"
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
@@ -6,21 +7,34 @@
 
 // You can delete this file if you're not using it
 
-const GTMNoScript = () => {
-  return (
-    <>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-W6LJZSJ"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-        ></iframe>
-      </noscript>
-    </>
-  )
-}
+// const GTMNoScript = () => {
+//   return (
+//     <noscript>
+//       <iframe
+//         src="https://www.googletagmanager.com/ns.html?id=GTM-W6LJZSJ"
+//         height="0"
+//         width="0"
+//         style="display:none;visibility:hidden"
+//       ></iframe>
+//     </noscript>
+//   )
+// }
 
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([GTMNoScript])
-}
+// const CookieBanner = () => {
+//   const initBanner = () => {
+//     var s = document.createElement("script")
+//     s.type = "text/javascript"
+//     s.async = true
+//     s.src = "https://app.termly.io/embed.min.js"
+//     s.id = "dee08c58-630a-4f90-8bd5-8af05cf976e6"
+//     s.setAttribute("data-name", "termly-embed-banner")
+//     var x = document.getElementsByTagName("script")[0]
+//     x.parentNode.insertBefore(s, x)
+//   }
+
+//   return <script>{initBanner()}</script>
+// }
+
+// export const onRenderBody = ({ setPreBodyComponents }) => {
+//   setPreBodyComponents([CookieBanner, GTMNoScript])
+// }
