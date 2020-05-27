ef4b1d95069ddf29a31ba16b0a04540c6b996e8b
George Richards <georgejjrichards@gmail.com>
2020-05-27 16:50:02 +0100

    styling blog page

--- src/templates/blog/item/index.tsx
+++ src/templates/blog/item/index.tsx
@@ -3,25 +3,38 @@ import Hero from "~/components/configurable/Hero"
 import Heading from "~/components/configurable/Heading"
 import Block from "~/components/configurable/Block"
 
+import "./styles.scss"
+
 // gatsby-node passes in data as context variable
 const BlogItem: FunctionComponent<any> = context => {
   // this grabs all the needed variables from within content.pageContext
-  const { title, body, image_hero } = context.pageContext
+  const { date, title, hero_title, body, image_hero } = context.pageContext
 
   return (
-    <>
+    <div className="blog-item">
       {!!image_hero?.publicURL && (
         <Hero image={image_hero?.publicURL} compact>
           <Heading level={1} underlined>
-            <span dangerouslySetInnerHTML={{ __html: title }} />
+            <span dangerouslySetInnerHTML={{ __html: hero_title }} />
           </Heading>
         </Hero>
       )}
 
       <Block>
-        <div className="container" dangerouslySetInnerHTML={{ __html: body }} />
+        <div className="container">
+          <Heading className="blog-item__heading" level={3}>
+            <span dangerouslySetInnerHTML={{ __html: title }} />
+          </Heading>
+          <div className="blog-item__info">
+            <span style={{ fontSize: 14 }}>{date}</span>
+          </div>
+          <div
+            style={{ marginTop: 16 }}
+            dangerouslySetInnerHTML={{ __html: body }}
+          />
+        </div>
       </Block>
-    </>
+    </div>
   )
 }
 
