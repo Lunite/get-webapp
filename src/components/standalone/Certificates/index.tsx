df12b8f73f4ff7c283603eccf4910f6f82c2f0e8
George Richards <georgejjrichards@gmail.com>
2020-06-05 17:12:09 +0100

    alt tags

--- src/components/standalone/Certificates/index.tsx
+++ src/components/standalone/Certificates/index.tsx
@@ -12,18 +12,41 @@ const Certificates = () => {
           Our Certifications
         </Heading>
         <p>
-        Quality is what we do. Green Energy Together has had its Quality Management System certified against a number of national and international quality standards. To attain and maintain these certifications, our business undertakes a vigorous programme to ensure the ongoing quality and improvement of our Management Systems. Review is undertaken by regular internal and external audits, conducted by our third-party certification partners.
+          Quality is what we do. Green Energy Together has had its Quality
+          Management System certified against a number of national and
+          international quality standards. To attain and maintain these
+          certifications, our business undertakes a vigorous programme to ensure
+          the ongoing quality and improvement of our Management Systems. Review
+          is undertaken by regular internal and external audits, conducted by
+          our third-party certification partners.
         </p>
-        <img className="hidden-xs" src="/images/certificates.png" />
+        <img
+          className="hidden-xs"
+          src="/images/certificates.png"
+          alt="Certificate Logos"
+        />
         <img
           className="visible-xs"
           style={{ maxWidth: "276px", margin: "0 auto" }}
           src="/images/certificates-mobile.jpg"
+          alt="Certificate Logos"
         />
-        <div style={{paddingTop: "20px", color: "grey", fontSize: "10px", margin:"auto", display: "flex", alignContent: "center" }}>
-        <p >
-        *All works carried out by the Green Energy Together brand operate under Solarplicity Smart Systems registration NAPIT 30902 and MCS NAP30902.
-        </p></div>
+        <div
+          style={{
+            paddingTop: "20px",
+            color: "grey",
+            fontSize: "10px",
+            margin: "auto",
+            display: "flex",
+            alignContent: "center",
+          }}
+        >
+          <p>
+            *All works carried out by the Green Energy Together brand operate
+            under Solarplicity Smart Systems registration NAPIT 30902 and MCS
+            NAP30902.
+          </p>
+        </div>
       </div>
     </Block>
   )
