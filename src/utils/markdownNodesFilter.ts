bd13e1d336ee149c80cfcb83bdfc50472a222935
George Richards <georgejjrichards@gmail.com>
2020-06-08 02:00:03 +0100

    fixing build

--- src/utils/markdownNodesFilter.ts
+++ src/utils/markdownNodesFilter.ts
@@ -1,4 +1,8 @@
 const markdownNodesFilter = (markdownNodes, parentSlug) => {
+  if (!markdownNodes?.length) {
+    return []
+  }
+
   return (
     markdownNodes.filter(node => {
       return node.fileAbsolutePath.indexOf(`/content/${parentSlug}/`) > -1
