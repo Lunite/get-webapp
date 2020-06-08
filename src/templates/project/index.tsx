86831f03a334a588575043cb31e87562a790c4f5
George Richards <georgejjrichards@gmail.com>
2020-06-08 11:05:02 +0100

    removing InfoStrip

--- src/templates/project/index.tsx
+++ src/templates/project/index.tsx
@@ -9,9 +9,16 @@ import InfoStrip from "~/components/configurable/InfoStrip"
 // gatsby-node passes in data as context variable
 const Project: FunctionComponent<any> = context => {
   // this grabs all the needed variables from within content.pageContext
-  const { title, description, image, image_hero, seo, info_strip } = context.pageContext
+  const {
+    title,
+    description,
+    image,
+    image_hero,
+    seo,
+    info_strip,
+  } = context.pageContext
 
-  const subheading = ''
+  const subheading = ""
 
   return (
     <>
@@ -30,28 +37,6 @@ const Project: FunctionComponent<any> = context => {
           </Col6>
         </div>
       </Block>
-      {Object.keys(info_strip).length && (<InfoStrip
-        location={info_strip.location}
-        dcPeak={info_strip.dc_peak}
-        developer={info_strip.developer}
-        inverters={info_strip.inverters}
-        modules={info_strip.modules}
-        mapUrl={info_strip.map_url}
-      />)}
-      {
-        // project.related_products && (
-        // <Block>
-        //   <Heading>Similar use cases:</Heading>
-        //   <Image
-        //     src="related-product-image.jpg"
-        //     title="Related Product Title"
-        //     hover={<p>related product info</p>}
-        //   />
-        //   TODO
-        //   {/** Loop through related projects */}
-        // </Block>
-        //)
-      }
     </>
   )
 }
