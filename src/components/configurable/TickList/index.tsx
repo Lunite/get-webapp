c2b28617663718f835a0fba6e5c12c38788830d0
George Richards <georgejjrichards@gmail.com>
2020-06-15 18:22:57 +0100

    animations r kool

--- src/components/configurable/TickList/index.tsx
+++ src/components/configurable/TickList/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react"
 
 import "./styles.scss"
+import Animate from "~/components/olc-framework/Animate"
 
 const TickList: FunctionComponent<any> = ({ nolines, blueticks, children }) => {
   return (
@@ -9,7 +10,16 @@ const TickList: FunctionComponent<any> = ({ nolines, blueticks, children }) => {
         blueticks ? "tick-list--blue-ticks" : ""
       }`}
     >
-      {children}
+      {React.Children.toArray(children).map((child, cIndex) => (
+        <Animate
+          properties={["opacity", "transform"]}
+          startValues={["0", "translateY(40px) rotate(0.5deg)"]}
+          endValues={["1", "translateY(0) rotate(0deg)"]}
+          delay={`${cIndex / 4}s`}
+        >
+          {React.cloneElement(child)}
+        </Animate>
+      ))}
     </ul>
   )
 }
