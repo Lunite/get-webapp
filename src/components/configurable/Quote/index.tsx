4d54b0e5b991c26346bc827516a8bd009dc11e45
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-07-22 22:41:56 +0100

    Refactored postcode field validation to pure html forms

--- src/components/configurable/Quote/index.tsx
+++ src/components/configurable/Quote/index.tsx
@@ -6,31 +6,15 @@ import { trackCustomEvent } from "gatsby-plugin-google-analytics"
 
 import "./styles.scss"
 
-const postcodeRegex = new RegExp(
-  "^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
-)
-
 const Quote: FunctionComponent<any> = ({
   title = "Get a quote today.",
   description = "It only takes 2 minutes to request a no-obligation quote, customised to you and your home's needs. Don't miss the chance to win your solar PV system for FREE.",
   ctaText = "Request Quote",
 }) => {
   const [postcode, setPostcode] = useState("")
-  const [valid, setValid] = useState(true)
-
-  useEffect(() => {
-    if (postcodeRegex.test(postcode)) {
-      setValid(true)
-    } else {
-      setValid(false)
-    }
-  }, [postcode])
 
   const handleSubmit = event => {
     event.preventDefault()
-    if (!valid || postcode === "") {
-      return 1
-    }
 
     const eventData = {
       category: "Form",
@@ -60,8 +44,10 @@ const Quote: FunctionComponent<any> = ({
         <div className="form__inner" style={{ filter: "blur(0px)" }}>
           <div className="form__fields">
             <input
-              className="form__text-input"
-              id="postcode-input"
+              required
+              pattern="^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
+              title="Please enter a valid UK postcode"
+              className="form__text-input postcode-input"
               type="text"
               placeholder="Enter your postcode..."
               name="postcode"
@@ -70,12 +56,6 @@ const Quote: FunctionComponent<any> = ({
                 setPostcode(e.target.value.toUpperCase())
               }}
             />
-            <p
-              className="error-text"
-              style={{ opacity: valid || postcode === "" ? "0" : "1" }}
-            >
-              Please enter a valid postcode
-            </p>
           </div>
           <div className="form__actions">
             <BlockCTA submit inline>
