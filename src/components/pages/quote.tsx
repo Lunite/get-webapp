1dda9e1463b272bb36f9d9b735d22a4c035b41d7
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-08-03 22:51:13 +0100

    Build logic for paginating form

--- src/components/pages/quote.tsx
+++ src/components/pages/quote.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react"
+import React, { useState, FormEvent, useEffect } from "react"
 import Hero from "../configurable/Hero"
 import Heading from "../configurable/Heading"
 import QuestionPage from "../configurable/QuoteTool"
@@ -10,6 +10,8 @@ import "./quote.scss"
 import Col6 from "../grid/Col6"
 import FormInput from "../olc-framework/FormInput"
 import BlockCTA from "../configurable/BlockCTA"
+import { imageNodesFilter } from "~/utils"
+import { PageProps } from "gatsby"
 
 interface IQuoteFormValues {
   name: string
@@ -63,10 +65,51 @@ const defaultValues: IQuoteFormValues = {
   },
 }
 
-const QuotePage: React.FC<{ location }> = props => {
-  const [formValues, setFormValues] = useState<IQuoteFormValues>(defaultValues)
+const QuotePage: React.FC<PageProps> = props => {
+  const [formValues, setFormValues] = useState<IQuoteFormValues>({
+    ...defaultValues,
+    ...props.location.state,
+  })
+  const [page, setPage] = useState(0)
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    if (page !== pages - 1) {
+      setPage(page + 1)
+    }
+  }
+
+  const pages = 1
+
+  const getPage = () => {
+    switch (page) {
+      case 0:
+        return (
+          <div className="row">
+            <Col6>
+              <Heading level={3}>Enter your postcode to get started</Heading>
+            </Col6>
+            <Col6>
+              <FormInput
+                name="postcode"
+                label="Postcode"
+                placeholder="Enter postcode..."
+                required
+                pattern="^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
+                title="Please enter a valid UK postcode"
+              />
+              <div className="form__actions">
+                <BlockCTA large submit className="fl-r">
+                  Get Started
+                </BlockCTA>
+              </div>
+            </Col6>
+          </div>
+        )
+        break
+
+      default:
+        return <>No Page Here :)</>
+    }
   }
   return (
     <div className="quote-page">
@@ -78,34 +121,12 @@ const QuotePage: React.FC<{ location }> = props => {
       <Block>
         <div className="container u-layout--indent container--column">
           <Col9>
-            {" "}
             <form
               name="quote-form"
               onSubmit={handleSubmit}
               className="form form--full-width"
             >
-              <div className="row">
-                <Col6>
-                  <Heading level={3}>
-                    Enter your postcode to get started
-                  </Heading>
-                </Col6>
-                <Col6>
-                  <FormInput
-                    name="postcode"
-                    label="Postcode"
-                    placeholder="Enter postcode..."
-                    required
-                    pattern="^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
-                    title="Please enter a valid UK postcode"
-                  />
-                  <div className="form__actions">
-                    <BlockCTA large submit className="align-end">
-                      Get Started
-                    </BlockCTA>
-                  </div>
-                </Col6>
-              </div>
+              {getPage()}
             </form>
           </Col9>
         </div>
