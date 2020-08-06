0c550dc6754007b1f12754755f7ac8d3461d8811
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-08-06 23:04:57 +0100

    Used shoutout on quote page

--- src/components/pages/quote.tsx
+++ src/components/pages/quote.tsx
@@ -9,7 +9,7 @@ import "./quote.scss"
 import Col6 from "../grid/Col6"
 import FormInput from "../olc-framework/FormInput"
 import BlockCTA from "../configurable/BlockCTA"
-import { imageNodesFilter } from "~/utils"
+import Image from "../configurable/Image"
 import { PageProps } from "gatsby"
 import InteractiveMap from "../configurable/QuoteTool/InteractiveMap"
 import Col11 from "../grid/Col11"
@@ -17,6 +17,7 @@ import Col12 from "../grid/Col12"
 import { number } from "prop-types"
 import { fromAddress, fromLatLong } from "../util/Quote/mapUtils"
 import RadioGrid from "../configurable/QuoteTool/RadioGrid"
+import Shoutout from "../configurable/Shoutout"
 
 interface IQuoteFormValues {
   name: string
@@ -113,29 +114,30 @@ const QuotePage: React.FC<PageProps> = props => {
     switch (page) {
       case 0:
         return (
-          <div className="row">
-            <Col6>
-              <Heading level={3}>Enter your postcode to get started</Heading>
-            </Col6>
-            <Col6>
-              <FormInput
-                name="postcode"
-                label="Postcode"
-                placeholder="Enter postcode..."
-                required
-                pattern="^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
-                title="Please enter a valid UK postcode"
-                value={formValues.postcode}
-                onChange={updatePostcode}
-                id="postcode"
-              />
-              <div className="form__actions">
-                <BlockCTA large submit className="fl-r">
-                  Get Started
-                </BlockCTA>
-              </div>
-            </Col6>
-          </div>
+          <Shoutout
+            image={<Image src="/images/products-bulb.jpg" title="Products" />}
+            text={
+              <>
+                <Heading underlined>Enter your postcode to get started</Heading>
+                <FormInput
+                  name="postcode"
+                  label="Postcode"
+                  placeholder="Enter postcode..."
+                  required
+                  pattern="^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})"
+                  title="Please enter a valid UK postcode"
+                  value={formValues.postcode}
+                  onChange={updatePostcode}
+                  id="postcode"
+                />
+                <div className="form__actions">
+                  <BlockCTA large submit className="fl-r">
+                    Get Started
+                  </BlockCTA>
+                </div>
+              </>
+            }
+          />
         )
       case 1:
         return (
