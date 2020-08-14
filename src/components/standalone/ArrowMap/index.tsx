b2f5a72b4aa441d598283d4e5f7dc231a855d10b
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-08-14 20:22:51 +0100

    Got arrow moving on mouse move

--- src/components/standalone/ArrowMap/index.tsx
+++ src/components/standalone/ArrowMap/index.tsx
@@ -10,6 +10,19 @@ interface ArrowMapProps {
   }
 }
 
+// Throttles function invocation to once every delay
+const throttle = (callback: Function, delay: number) => {
+  let previousCall = new Date().getTime()
+  return (...args) => {
+    const time = new Date().getTime()
+
+    if (time - previousCall >= delay) {
+      previousCall = time
+      callback(...args)
+    }
+  }
+}
+
 const ArrowMap: React.FC<ArrowMapProps> = props => {
   const mapOptions = {
     mapTypeControl: false,
@@ -20,30 +33,49 @@ const ArrowMap: React.FC<ArrowMapProps> = props => {
   }
 
   // State Hooks
-  const [mouseDown, setMouseDown] = useState<boolean>(false)
-  const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({
+  const [mouseState, setMouseState] = useState<{
+    x: number
+    y: number
+    down: boolean
+  }>({
     x: 0,
     y: 0,
+    down: false,
   })
-  const [azimuth, setAzimuth] = useState<number>(0)
 
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      const center = { x: canvas.width / 2, y: canvas.height / 2 }
+      const clientCenter = {
+        x: canvas.clientWidth / 2,
+        y: canvas.clientHeight / 2,
+      }
+      console.log("canvas center", clientCenter)
+      const r = canvas.height / 3
+      let theta = Math.atan2(
+        mouseState.x - clientCenter.x,
+        mouseState.y - clientCenter.y
+      )
+      theta = -theta + Math.PI
+      console.log("angle is", theta * (180 / Math.PI) - 180)
+      drawArrowFromAngle(center.x, center.y, r, theta - Math.PI / 2)
+    }
+  }, [mouseState.x, mouseState.y])
+
   // Event Handler Functions
   const onMouseDown = (e: MouseEvent) => {
-    setMousePosition({ x: e.offsetX, y: e.offsetY })
-    setMouseDown(true)
+    setMouseState({ x: e.offsetX, y: e.offsetY, down: true })
   }
 
   const onMouseUp = (e: MouseEvent) => {
-    setMousePosition({ x: e.offsetX, y: e.offsetY })
-    setMouseDown(false)
+    setMouseState({ x: e.offsetX, y: e.offsetY, down: false })
   }
 
   const onMouseMove = (e: MouseEvent) => {
-    if (mouseDown) {
-      setMousePosition({ x: e.offsetX, y: e.offsetY }) // use useEffect for callback from this, adjust arrow position apropriately
-    }
+    setMouseState({ x: e.offsetX, y: e.offsetY, down: true }) // use useEffect for callback from this, adjust arrow position apropriately
   }
 
   // Canvas update functions
@@ -53,14 +85,14 @@ const ArrowMap: React.FC<ArrowMapProps> = props => {
     y: number,
     r: number,
     theta: number,
-    aWidth: number,
-    aLength: number
+    aWidth = 10,
+    aLength = 10
   ) => {
     const ctx = canvasRef.current.getContext("2d")
 
     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
     ctx.translate(x, y)
-    ctx.rotate(theta - Math.PI / 2)
+    ctx.rotate(theta)
     ctx.beginPath()
     ctx.moveTo(0, 0)
     ctx.lineTo(r, 0)
@@ -77,72 +109,18 @@ const ArrowMap: React.FC<ArrowMapProps> = props => {
     ctx.setTransform(1, 0, 0, 1, 0, 0)
   }
 
-  const drawArrow = (
-    x0: number,
-    y0: number,
-    x1: number,
-    y1: number,
-    aWidth: number,
-    aLength: number
-  ) => {
-    var dx = x1 - x0
-    var dy = y1 - y0
-    var angle = Math.atan2(dy, dx)
-    var length = Math.sqrt(dx * dx + dy * dy)
-
-    const ctx = canvasRef.current.getContext("2d")
-
-    //
-    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
-
-    //
-    ctx.translate(x0, y0)
-    ctx.rotate(angle)
-    ctx.beginPath()
-    ctx.moveTo(0, 0)
-    ctx.lineTo(length, 0)
-    ctx.moveTo(length - aLength, -aWidth)
-    ctx.lineTo(length, 0)
-    ctx.lineTo(length - aLength, aWidth)
-
-    //
-    ctx.fillStyle = "#3c96c5"
-    ctx.lineWidth = 5
-    ctx.strokeStyle = "#3c96c5"
-    ctx.lineJoin = "miter"
-
-    //
-    ctx.stroke()
-    ctx.setTransform(1, 0, 0, 1, 0, 0)
-  }
-
-  // // Throttles function invocation to once every delay
-  // const throttle = (callback: CallableFunction, delay: number) => {
-  //   let previousCall = new Date().getTime()
-  //   return () => {
-  //     const time = new Date().getTime()
-
-  //     if (time - previousCall >= delay) {
-  //       previousCall = time
-  //       callback(...callback.arguments)
-  //     }
-  //   }
-  // }
-
   useEffect(() => {
     // Configures canvasRef event listeners
     if (canvasRef) {
       const canvas = canvasRef.current
       canvas.addEventListener("mousedown", onMouseDown, false)
       canvas.addEventListener("mouseup", onMouseUp, false)
-      canvas.addEventListener("mousemove", onMouseMove, false)
-      canvas.addEventListener("touchstart", onMouseDown, false)
-      canvas.addEventListener("touchend", onMouseUp, false)
-      canvas.addEventListener("touchmove", onMouseMove, false)
+      canvas.addEventListener("mouseout", onMouseUp, false)
+      canvas.addEventListener("mousemove", throttle(onMouseMove, 5), false)
 
       // Draws initial arrow at 0 degrees on canvas
       const center = { x: canvas.width / 2, y: canvas.height / 2 }
-      drawArrowFromAngle(center.x, center.y, canvas.height / 3, 0, 10, 10)
+      drawArrowFromAngle(center.x, center.y, canvas.height / 3, 0)
     }
   }, [canvasRef])
 
